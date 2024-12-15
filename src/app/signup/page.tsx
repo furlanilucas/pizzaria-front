fc90d6c71da41e185ff23df99ca19437bfcb58c2
furlanilucas <furlani.lucas7@gmail.com>
2024-12-15 16:36:04 -0300

    Implementação tela de cadastro

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../page.module.scss';
+import logoImg from '/public/logo.svg';
+
 export default function Signup(){
     return(
         <>
-        <main>
-            <h1>Pagina Cadastro</h1>
-        </main>
+       <div className={styles.containerCenter}>
+      <Image 
+        src={logoImg} 
+        alt="Logo da pizzaria" 
+      />
+
+      <section className={styles.login}>
+        <h1>Criando sua conta</h1>
+        <form>
+        <input 
+            type="text" 
+            required 
+            name="name" 
+            placeholder="Digite seu nome" 
+            className={styles.input} 
+          />
+          <input 
+            type="email" 
+            required 
+            name="email" 
+            placeholder="Digite seu email..." 
+            className={styles.input} 
+          />
+          <input 
+            type="password" 
+            required 
+            name="password" 
+            placeholder="**************" 
+            className={styles.input} 
+          />
+          <button type="submit" className={styles.button}>Cadastrar</button>
+        </form>
+
+        <Link href="/" className={styles.text}>
+          Já possui uma conta?
+        </Link>
+      </section>
+    </div>
         </>
     )
 }
