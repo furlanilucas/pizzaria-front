c1f8499f7672b61c39bd4de3a8214a60117291b4
furlanilucas <furlani.lucas7@gmail.com>
2024-12-16 09:23:49 -0300

    Implementação da funcionalidade de login utilizando handleLogin, com integração à API via requisição POST para /session e redirecionamento ao dashboard. Ajustes no formulário de login para vincular a ação ao handleLogin. Implementação da funcionalidade de cadastro utilizando handleRegister, com integração à API via requisição POST para /users e redirecionamento à página inicial após sucesso. Validações de campos obrigatórios para os formulários de login e cadastro. Adicionados imports das funções api e redirect para suportar as novas funcionalidades. Essas mudanças integram o frontend com o backend para autenticação e criação de usuários, melhorando a funcionalidade e usabilidade do sistema.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,41 @@ import styles from './page.module.scss';
 import logoImg from '/public/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { api } from '@/services/api';
+import { redirect } from 'next/navigation';
 
 
 export default function Page() {
+  async function handleLogin(formData: FormData){
+    "use server"
+
+    const email = formData.get("email");
+    const password = formData.get("password")
+
+    if(email === "" || password ===" "){
+      
+      return;
+    }
+    
+    try{
+    
+      const response = await api.post("/session",{
+        email,
+        password
+    })
+
+    if(!response.data.token){
+      return;
+    }
+
+    console.log(response.data)
+
+    }catch(err){
+      console.log(err)
+      return;
+    }
+    redirect("/dashboard")
+  }
   return (
     <div className={styles.containerCenter}>
       <Image 
@@ -13,7 +45,7 @@ export default function Page() {
       />
 
       <section className={styles.login}>
-        <form>
+        <form action={handleLogin}>
           <input 
             type="email" 
             required 
